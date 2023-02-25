7bafc19894410c85c4f735bc64aeb903bb6834bf
Marian Estigarribia <mnelugo@gmail.com>
2023-02-25 00:10:08 -0300

    Agregue FormSecurePlace

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1> Hola Mundo!</h1>
+      <FontAwesomeIcon icon={faGlobe}/> 
+
+    </div>
+  );
+}
+
+export default App;
