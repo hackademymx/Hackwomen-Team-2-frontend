04ab3b8cc748f32cc783af4f13a37528cdeed187
Marian Estigarribia <mnelugo@gmail.com>
2023-02-26 21:32:53 -0300

    Formulario Lugar Seguro

--- src/components/FormSecurePlace/FormSecurePlace.jsx
+++ src/components/FormSecurePlace/FormSecurePlace.jsx
@@ -1,15 +1,93 @@
-import React from "react";
-import {DivFormSecurePlace, FormFormSecurePlace, TextFieldFormSecurePlace} from "./FormSecurePlaceStyles"
+import React,{useState} from "react";
+import {DivFormSecurePlace, 
+    FormFormSecurePlace, 
+    TextFieldFormSecurePlace,
+    ButtonFormSecurePlace,
+} from "./FormSecurePlaceStyles"
 
 export default function FormSecurePlace(){
+    const [form, setForm]= useState({
+        name:"",
+        description:"",
+        state:"",
+        street:"",
+        suburb:"",
+        number:""
+    });
+
+    const [loading, setLoading] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setLoading(true);
+        console.log(form);
+    };
+
+    const handleChange = (e) => {
+        const nameInput=e.target.name;
+        const valueInput=e.target.value;
+
+        const internalForm=form;
+
+        internalForm[nameInput]= valueInput;
+    
+        setForm(internalForm);
+    };
+
     return (
     <DivFormSecurePlace>
-        <FormFormSecurePlace>
+        <FormFormSecurePlace onSubmit={handleSubmit}>
+            <h1>Lugares Seguros</h1>
             <TextFieldFormSecurePlace
-            id="outlined-basic" 
-            label="Outlined" 
-            variant="outlined"
+                id="outlined-basic" 
+                label="Nombre" 
+                name="name"
+                variant="outlined"
+                onChange={handleChange}
             />
+            <TextFieldFormSecurePlace
+                id="outlined-basic" 
+                label="Descripción" 
+                name="description"
+                variant="outlined"
+                onChange={handleChange}
+            />
+            <TextFieldFormSecurePlace
+                id="outlined-basic" 
+                label="Estado" 
+                name="state"
+                variant="outlined"
+                onChange={handleChange}
+            />
+            <TextFieldFormSecurePlace
+                id="outlined-basic" 
+                label="Calle" 
+                name="street"
+                variant="outlined"
+                onChange={handleChange}
+            />
+            <TextFieldFormSecurePlace
+                id="outlined-basic" 
+                label="Número" 
+                name="number"
+                variant="outlined"
+                onChange={handleChange}
+            />
+            <TextFieldFormSecurePlace
+                id="outlined-basic" 
+                label="Colonia" 
+                name="suburb"
+                variant="outlined"
+                onChange={handleChange}
+            />
+            <ButtonFormSecurePlace 
+                type="submit" 
+                variant="contained" 
+                color="primary"
+                desabled={loading}
+            >
+             {loading?"Enviando datos...":"Enviar Datos"}
+            </ButtonFormSecurePlace>
         </FormFormSecurePlace>
     </DivFormSecurePlace>
     );
--- src/components/FormSecurePlace/FormSecurePlaceStyles.jsx
+++ src/components/FormSecurePlace/FormSecurePlaceStyles.jsx
@@ -1,14 +1,25 @@
 import { styled } from "@mui/material/styles"
-import { TextField } from "@mui/material"
+import { TextField, Button } from "@mui/material"
 
 export const DivFormSecurePlace=styled("div")({
     display:"flex",
     justifyContent:"center",
     alignItems:"center"
-})
+});
 
-export const FormFormSecurePlace=styled("form")({})
+export const FormFormSecurePlace=styled("form")({
+    marginTop:"10vh",
+    padding:"20px 0 20px 0",
+    width:"450px",
+    minHeight:"500px",
+    border:"1px solid black"
+});
 
 export const TextFieldFormSecurePlace=styled(TextField)({
+    width: "90%",
+    marginBottom:"10px",
+});
+
+export const ButtonFormSecurePlace=styled(Button)({
     width: "90%",
 })
